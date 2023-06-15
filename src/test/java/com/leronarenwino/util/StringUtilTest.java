74ce6c81063536fb662476d1218d691c3d766b1a
Francisco Dueñas <fduenas@bancolombia.com.co>
2023-06-15 10:53:32 -0500

    :zap: [UPDATE] Implementation of different test cases

--- src/test/java/com/leronarenwino/util/StringUtilTest.java
+++ src/test/java/com/leronarenwino/util/StringUtilTest.java
@@ -6,11 +6,23 @@ import org.junit.Test;
 public class StringUtilTest {
 
     @Test
-    public void testRepeat() {
-
-        Assert.assertEquals("HelloHelloHello", StringUtil.repeat("Hello", 3));
+    public void repeat_string_once() {
         Assert.assertEquals("Hello", StringUtil.repeat("Hello", 1));
+    }
 
+    @Test
+    public void repeat_string_multiple_times() {
+        Assert.assertEquals("HelloHelloHello", StringUtil.repeat("Hello", 3));
+    }
+
+    @Test
+    public void repeat_string_zero_times() {
+        Assert.assertEquals("", StringUtil.repeat("Hello", 0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_string_negative_times() {
+        StringUtil.repeat("Hello", -1);
     }
 
 }
