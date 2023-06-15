17eb4f27e8142969b47dd15cb7db240b831679f8
Francisco Dueñas <fduenas@bancolombia.com.co>
2023-06-15 16:31:19 -0500

    :zap: [UPDATE] Add test method

--- src/test/java/com/leronarenwino/player/PlayerTest.java
+++ src/test/java/com/leronarenwino/player/PlayerTest.java
@@ -16,4 +16,15 @@ public class PlayerTest {
         assertFalse(player.play());
 
     }
+
+    @Test
+    public void wins_when_dice_number_is_big() {
+        Dice dice = Mockito.mock(Dice.class);
+        Mockito.when(dice.roll()).thenReturn(4);
+
+        Player player = new Player(dice, 3);
+        assertTrue(player.play());
+
+    }
+
 }
