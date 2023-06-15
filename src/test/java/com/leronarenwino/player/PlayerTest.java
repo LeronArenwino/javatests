5685598504993f4c28bf388ddaccf9de4cbcc908
Francisco Dueñas <fduenas@bancolombia.com.co>
2023-06-15 16:28:37 -0500

    :zap: [NEW] Add Dice, Player and PlayerTest classes

---
+++ src/test/java/com/leronarenwino/player/PlayerTest.java
@@ -0,0 +1,19 @@
+package com.leronarenwino.player;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void lose_when_dice_number_is_too_low() {
+        Dice dice = Mockito.mock(Dice.class);
+        Mockito.when(dice.roll()).thenReturn(2);
+
+        Player player = new Player(dice, 3);
+        assertFalse(player.play());
+
+    }
+}
