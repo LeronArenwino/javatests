c23616ee1a6cb891aabe9aa81b82372096b1a8b4
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-07-16 10:51:58 -0500

    :zap: [UPDATE] Add roman numerals to 1000

--- src/main/java/com/leronarenwino/util/RomanNumerals.java
+++ src/main/java/com/leronarenwino/util/RomanNumerals.java
@@ -6,12 +6,52 @@ public class RomanNumerals {
 
         StringBuilder result = new StringBuilder();
 
+        while (number >= 1000){
+            result.append("M");
+            number -= 1000;
+        }
+
+        if (number >= 900) {
+            result.append("CM");
+            number -= 900;
+        }
+
+        if (number >= 500) {
+            result.append("D");
+            number -= 500;
+        }
+
+        if (number >= 400) {
+            result.append("CD");
+            number -= 400;
+        }
+
+        while (number >= 100){
+            result.append("C");
+            number -= 100;
+        }
+
+        if (number >= 90) {
+            result.append("XC");
+            number -= 90;
+        }
+
+        if (number >= 50) {
+            result.append("L");
+            number -= 50;
+        }
+
+        if (number >= 40) {
+            result.append("XL");
+            number -= 40;
+        }
+
         while (number >= 10){
             result.append("X");
             number -= 10;
         }
 
-        if (number >= 9) {
+        if (number == 9) {
             result.append("IX");
             number -= 9;
         }
