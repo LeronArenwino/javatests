97026b4566a1b649888b115c51fce0a424c856ac
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-07-16 11:23:38 -0500

    :zap: [UPDATE] Movie repository

--- src/main/java/com/leronarenwino/movies/data/MovieRepository.java
+++ src/main/java/com/leronarenwino/movies/data/MovieRepository.java
@@ -1,4 +1,13 @@
 package com.leronarenwino.movies.data;
 
+import com.leronarenwino.movies.model.Movie;
+
+import java.util.Collection;
+
 public interface MovieRepository {
+
+    Movie findById(long id);
+    Collection<Movie> findAll();
+    void saveOrUpdate(Movie movie);
+
 }
