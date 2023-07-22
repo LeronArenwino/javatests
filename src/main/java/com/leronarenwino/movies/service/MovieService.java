01c4f1f17e7461c206db324d4730ea7a4b74578c
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-07-22 06:25:25 -0500

    :zap: [UPDATE] Add findMoviesByGenre method

--- src/main/java/com/leronarenwino/movies/service/MovieService.java
+++ src/main/java/com/leronarenwino/movies/service/MovieService.java
@@ -1,4 +1,22 @@
 package com.leronarenwino.movies.service;
 
+import com.leronarenwino.movies.data.MovieRepository;
+import com.leronarenwino.movies.model.Genre;
+import com.leronarenwino.movies.model.Movie;
+
+import java.util.Collection;
+
 public class MovieService {
+
+    private final MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public Collection<Movie> findMoviesByGenre(Genre genre) {
+        return movieRepository.findAll().stream()
+                .filter(movie -> movie.getGenre() == genre)
+                .toList();
+    }
 }
