080c3922e90827aa2c08b8231f1877c6d21fdb89
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-07-25 20:55:20 -0500

    :zap: [UPDATE] New tests

--- src/test/java/com/leronarenwino/movies/service/MovieServiceTest.java
+++ src/test/java/com/leronarenwino/movies/service/MovieServiceTest.java
@@ -3,19 +3,22 @@ package com.leronarenwino.movies.service;
 import com.leronarenwino.movies.data.MovieRepository;
 import com.leronarenwino.movies.model.Genre;
 import com.leronarenwino.movies.model.Movie;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MovieServiceTest {
 
-    @Test
-    void return_movies_by_genre() {
+    private MovieService movieService;
 
+    @BeforeEach
+    void setUp() {
         MovieRepository movieRepository = Mockito.mock(MovieRepository.class);
         Mockito.when(movieRepository.findAll()).thenReturn(
                 Arrays.asList(
@@ -29,10 +32,24 @@ class MovieServiceTest {
                 )
         );
 
-        MovieService movieService = new MovieService(movieRepository);
-        List<Integer> movies = movieService.findMoviesByGenre(Genre.COMEDY)
-                        .stream().map(Movie::getId)
-                        .toList();
-        assertEquals(movies, Arrays.asList(3, 6));
+        movieService = new MovieService(movieRepository);
+    }
+
+    @Test
+    void return_movies_by_genre() {
+
+        Collection<Movie> movies = movieService.findMoviesByGenre(Genre.COMEDY);
+        assertEquals(getMoviesIds(movies), Arrays.asList(3, 6));
+    }
+
+    @Test
+    void return_movies_by_length() {
+        Collection<Movie> movies  = movieService.findMoviesByLength(119);
+        assertEquals(getMoviesIds(movies), Arrays.asList(2, 3, 4, 5, 6));
+    }
+
+    private static List<Integer> getMoviesIds(Collection<Movie> movies) {
+        return movies.stream().map(Movie::getId)
+                .toList();
     }
 }
