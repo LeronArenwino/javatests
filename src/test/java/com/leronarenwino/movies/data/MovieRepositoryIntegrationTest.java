062eee968a40042d6499620323e069a706893a31
Francisco Dueñas <frsduenasca@unal.edu.co>
2023-07-27 00:14:04 -0500

    :zap: [UPDATE] Add insert_a_movie test

--- src/test/java/com/leronarenwino/movies/data/MovieRepositoryIntegrationTest.java
+++ src/test/java/com/leronarenwino/movies/data/MovieRepositoryIntegrationTest.java
@@ -1,34 +1,44 @@
 package com.leronarenwino.movies.data;
 
-import com.leronarenwino.movies.model.*;
-import org.junit.jupiter.api.BeforeAll;
+import com.leronarenwino.movies.model.Genre;
+import com.leronarenwino.movies.model.Movie;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
 
-import javax.sql.DataSource;
-
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
 import java.util.Collection;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MovieRepositoryIntegrationTest {
 
-    private static MovieRepositoryJdbc movieRepository;
+    private MovieRepositoryJdbc movieRepository;
+    private DriverManagerDataSource dataSource;
 
-    @BeforeAll
-    static void setUp() throws SQLException {
-        DataSource dataSource = new DriverManagerDataSource("jdbc:h2:mem:test;MODE=MYSQL", "sa", "sa");
+    @BeforeEach
+    void setUp() throws SQLException {
+        dataSource = new DriverManagerDataSource("jdbc:h2:mem:test;MODE=MYSQL", "sa", "sa");
 
         ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("sql-scripts/test-data.sql"));
 
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 
         movieRepository = new MovieRepositoryJdbc(jdbcTemplate);
+    }
+
+    @AfterEach
+    void tearDown() throws SQLException {
+
+        final Statement statement = dataSource.getConnection().createStatement();
+        statement.execute("DROP ALL objects DELETE files");
+
     }
 
     @Test
@@ -41,6 +51,19 @@ class MovieRepositoryIntegrationTest {
                 new Movie(2, "Memento", 113, Genre.THRILLER),
                 new Movie(3, "Matrix", 136, Genre.ACTION)
         ));
+    }
+
+    @Test
+    void insert_a_movie() {
+
+        Movie movie = new Movie("Super 8", 112, Genre.THRILLER);
+
+        movieRepository.saveOrUpdate(movie);
+
+        Movie movieFromDb = movieRepository.findById(4);
+
+        assertEquals(movieFromDb, new Movie(4, "Super 8", 112, Genre.THRILLER));
+
     }
 
     @Test
